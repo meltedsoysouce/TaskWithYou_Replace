9d6720b68b81d9d7ef599aff7685e6ae8fa602b7
Mchi_yuki <metalsngke@gmail.com>
2023-11-11 16:12:49 +0900

    CardListのViewModelを実装

---
+++ TaskWithYou/Shared/VIewModels/Cards/CardListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskWithYou.Shared.VIewModels.Cards
+{
+    public class CardListViewModel
+    {
+        public List<CardListItem> CardList { get; set; }
+    }
+
+    public class CardListItem
+    {
+        public Guid Uid { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime CreateAt { get;set; }
+
+        public DateTime? DeadLine { get; set; }
+
+        public string Description { get; set; }
+}
