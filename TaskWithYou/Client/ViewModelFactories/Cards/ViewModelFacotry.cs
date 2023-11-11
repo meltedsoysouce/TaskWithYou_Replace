fcac413db012c096472e9797055d1d5b1cf6f092
Mchi_yuki <metalsngke@gmail.com>
2023-11-11 16:13:07 +0900

    CardListのViewModelを実装

---
+++ TaskWithYou/Client/ViewModelFactories/Cards/ViewModelFacotry.cs
@@ -0,0 +1,31 @@
+﻿using TaskWithYou.Shared.VIewModels.Cards;
+
+namespace TaskWithYou.Client.ViewModelFactories.Cards
+{
+    public interface IViewModelFactory
+    {
+        CardListViewModel GetList();
+    }
+
+    public class ViewModelFacotry : IViewModelFactory
+    {
+        public CardListViewModel GetList()
+        {
+            CardListViewModel viewModel = new ();
+            viewModel.CardList = Enumerable.Range(0, 5)
+                .Select(x =>
+                {
+                    return new CardListItem()
+                    {
+                        Uid = Guid.Empty,
+                        Title = $"test {x}",
+                        CreateAt = DateTime.Today,
+                        DeadLine = null,
+                        Description = $"This is a test {x}"
+                    };
+                })
+                .ToList();
+            return viewModel;
+        }
+    }
+}
