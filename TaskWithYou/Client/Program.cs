d99da883f91d5f0d1a0446589b27f2e2631d1eec
Mchi_yuki <metalsngke@gmail.com>
2023-11-11 16:17:43 +0900

    FluentUIの導入

--- TaskWithYou/Client/Program.cs
+++ TaskWithYou/Client/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
 using TaskWithYou.Client;
+using TaskWithYou.Client.ViewModelFactories.Cards;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,5 +16,11 @@ builder.Services.AddHttpClient("TaskWithYou.ServerAPI", client => client.BaseAdd
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("TaskWithYou.ServerAPI"));
 
 builder.Services.AddApiAuthorization();
+
+// FluentUI‚Ì“±“ü
+builder.Services.AddFluentUIComponents(options =>
+{
+    options.HostingModel = BlazorHostingModel.WebAssembly;
+});
 
 await builder.Build().RunAsync();
