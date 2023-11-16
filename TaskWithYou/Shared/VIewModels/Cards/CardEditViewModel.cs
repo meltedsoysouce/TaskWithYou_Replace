f339013bccded721717ba5ffebc557849ac48571
Mchi_yuki <metalsngke@gmail.com>
2023-11-16 21:21:06 +0900

    カード編集モーダルのViewModelを実装

---
+++ TaskWithYou/Shared/VIewModels/Cards/CardEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskWithYou.Shared.ViewModels.Cards
+{
+    public class CardEditViewModel
+    {
+        [Required]
+        public Guid Uid { get; init; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime CreateAt { get; set; }
+
+        public DateTime DeadLine { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
+
