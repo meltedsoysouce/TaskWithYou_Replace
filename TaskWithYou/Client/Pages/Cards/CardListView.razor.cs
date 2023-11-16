8b2e46e19ae7a9ea50243a1634ec735ee8325cf3
Mchi_yuki <metalsngke@gmail.com>
2023-11-16 12:11:06 +0900

    カードを削除するモーダルのViewを実装

--- TaskWithYou/Client/Pages/Cards/CardListView.razor.cs
+++ TaskWithYou/Client/Pages/Cards/CardListView.razor.cs
@@ -16,7 +16,7 @@ namespace TaskWithYou.Client.Pages.Cards
 
         private CardListViewModel ViewModel { get; set; }
 
-        private _CardCreateModal CardCreateModal { get; set; }
+        
 
         protected override void OnInitialized()
         {
@@ -24,6 +24,9 @@ namespace TaskWithYou.Client.Pages.Cards
             base.OnInitialized();
         }
 
+        // 将来的にはうまいことファイルを分割したい
+        #region カード作成モーダル
+        private _CardCreateModal CardCreateModal { get; set; }
         private async Task AsyncInvokeCreateCardModal()
         {
             var dialog = await DialogService
@@ -38,5 +41,25 @@ namespace TaskWithYou.Client.Pages.Cards
             var result = await dialog.Result;
             var data = result.Data as CardCreateViewModel;
         }
+        #endregion
+
+        #region カード削除モーダル
+        private _CardRemoveModal CardRemoveModal { get; set;}
+        private async Task AsyncInvokeRemoveCardModal()
+        {
+            var dialog = await DialogService
+                .ShowDialogAsync<_CardRemoveModal>(
+                    ViewModelFacotry.GetRemoveViewModel(Guid.Empty),
+                    new DialogParameters()
+                    {
+                        Title = "カードの新規作成",
+                        Modal = true
+                    }
+                );
+
+            var result = await dialog.Result;
+            var date = result.Data as CardRemoveViewModel;
+        }
+        #endregion
     }
 }
