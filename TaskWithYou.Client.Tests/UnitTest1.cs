63fa9c4b720b89c1b0e6b8b35616996f920bc2a4
Mchi_yuki <metalsngke@gmail.com>
2023-11-23 14:55:52 +0900

    ViewModelFactoryのテストを追加

---
+++ TaskWithYou.Client.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TaskWithYou.Client.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TaskWithYou.Client.Tests/ViewModelFactories/Cards/ViewModelFactoryTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskWithYou.Client.ViewModelFactories.Cards;
+using TaskWithYou.Shared.ViewModels.Cards;
+
+namespace TaskWithYou.Client.Tests.ViewModelFactories.Cards
+{
+    public class ViewModelFactoryTest
+    {
+        [Fact(DisplayName="CardListViewModelが出力されていることの確認")]
+        public void GetListValueTypeCheck()
+        {
+            ViewModelFacotry factory = new();
+            var listViewModel = factory.GetList();
+            Assert.IsType<CardListViewModel>(listViewModel);
+        }
+
+        [Fact(DisplayName="CardDetailViewModelが出力されているかの確認")]
+        public void GetDetailCheck()
+        {
+            ViewModelFacotry factory = new();
+            var detailViewModel = factory.GetDetail(Guid.Empty);
+            Assert.IsType<CardDetailViewModel>(detailViewModel);
+        }
+
+        [Fact(DisplayName = "CardCreateViewModelが出力されていることの確認")]
+        public void GenerateCreateCheck()
+        {
+            ViewModelFacotry factory = new();
+            var createViewModel = factory.GenerateCreateViewModel();
+            Assert.IsType<CardCreateViewModel>(createViewModel);
+        }
+
+        [Fact(DisplayName = "CardEditViewModelが出力されていることの確認")]
+        public void GetEditViewModelCheck()
+        {
+            ViewModelFacotry factory = new();
+            var editViewModel = factory.GetEditViewModel(Guid.Empty);
+            Assert.IsType<CardEditViewModel>(editViewModel);
+        }
+
+        [Fact(DisplayName = "CardRemoveViewModelが出力されていることの確認")]
+        public void GetRemoveViewModelTest()
+        {
+            ViewModelFacotry facotry = new();
+            var removeViewModel = facotry.GetRemoveViewModel(Guid.Empty);
+            Assert.IsType<CardRemoveViewModel>(removeViewModel);
+        }
+    }
+}
