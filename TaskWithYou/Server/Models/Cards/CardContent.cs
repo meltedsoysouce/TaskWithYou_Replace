ab9dad159de9bb683fd4b5ddd05506d6ebc11290
Mchi_yuki <metalsngke@gmail.com>
2023-11-23 14:59:52 +0900

    CardContentモデルを追加

---
+++ TaskWithYou/Server/Models/Cards/CardContent.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskWithYou.Server.Models.Cards
+{
+    /// <summary>
+    /// カードの内容を表現するクラス
+    /// </summary>
+    public class CardContent
+    {
+        public Guid Uid { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public DateTime? DeadLine { get; set; }
+
+        public string Description { get; set; }
+    }
+}
